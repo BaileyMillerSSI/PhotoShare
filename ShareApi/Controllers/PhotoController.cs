576b34954ee3de75606a8a7ba80324e0b443a229
Bailey Miller <bjm19@pct.edu>
2019-04-10 02:56:21 -0400

    It works for right now...

---
+++ ShareApi/Controllers/PhotoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShareApi.Models;
+
+namespace ShareApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<PhotoData>> Get()
+        {
+            return Ok();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] PhotoData value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ShareApi/Models/PhotoData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareApi.Models
+{
+    public class PhotoData
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+
+        public long Likes { get; set; }
+
+        public byte[] Photo { get; set; }
+
+    }
+}
